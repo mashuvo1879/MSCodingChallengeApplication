75eaa2808bc751a1c55f152e40736ccc58250c5b
Masud Shuvo <masud.shuvo@widespace.com>
2016-02-19 01:52:12 +0600

    Initial commit.

---
+++ MSCodingChallengeApplication/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MSCodingChallengeApplication
+//
+//  Created by Masud Shuvo on 2/19/16.
+//  Copyright © 2016 Masud Shuvo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MSCodingChallengeApplication/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MSCodingChallengeApplication
+//
+//  Created by Masud Shuvo on 2/19/16.
+//  Copyright © 2016 Masud Shuvo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
