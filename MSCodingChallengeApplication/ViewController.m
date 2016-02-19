8ce12fa2bdb6406abbcffcacb413984dd002fcc8
Masud Shuvo <masud.shuvo@widespace.com>
2016-02-19 19:19:58 +0600

    Revert "Initial commit with dummy framework."
    This reverts commit ef0f71db7d9bfcd4c97c78da788b25149bcfc290.

--- MSCodingChallengeApplication/ViewController.m
+++ MSCodingChallengeApplication/ViewController.m
@@ -7,60 +7,16 @@
 //
 
 #import "ViewController.h"
-#import <MSCodingChallenge/MSCodingChallenge.h>
-#import <MSCodingChallenge/MSObject.h>
 
-#define GetFileData(fileName, fileExtension) [NSData dataWithContentsOfFile:[[NSBundle bundleForClass:[self class]] pathForResource:fileName ofType:fileExtension]]
+@interface ViewController ()
 
-@interface ViewController () <MSCodingChallengeDelegate>
-@property (nonatomic, strong) MSCodingChallenge *codingChallenge;
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-//    self.codingChallenge = [[MSCodingChallenge alloc] initWithDelegate:self];
-//    [self.codingChallenge fetchObjects];
     // Do any additional setup after loading the view, typically from a nib.
-    
-//    NSString *filePath = [[NSBundle mainBundle] pathForResource:@"mockData" ofType:@"json"];
-//    
-//    NSError *error;
-//    
-//    NSString *fileContents = [NSString stringWithContentsOfFile:filePath encoding:NSUTF8StringEncoding error:&error];
-//    
-//    if(error) {
-//        NSLog(@"Error reading file: %@", error.localizedDescription);
-//    }
-//    
-//    // Get JSON objects into initial array
-//    NSArray *rawExhibits = (NSArray *)[NSJSONSerialization JSONObjectWithData:[fileContents dataUsingEncoding:NSUTF8StringEncoding] options:0 error:NULL];
-//
-//    NSLog(@"%@",[rawExhibits description]);
-//    NSMutableArray *myItems = [[NSMutableArray alloc] init];
-//    
-//    for (NSDictionary *object in rawExhibits) {
-//        NSString *objectName = object[@"Name"];
-//        NSString *objectType = object[@"Type"];
-//        
-//        // Create new exhibit object with data procured from JSON object
-//        W5BExhibit *exhibit = [[W5BExhibit alloc] initWithExhibitName:objectName exhibitType:objectType];
-//        
-//        [myItems addObject:exhibit];
-//    }
-//    
-//    self.allExhibits = [[NSArray alloc] initWithArray:myItems];
-
-    self.codingChallenge = [[MSCodingChallenge alloc] initWithDelegate:self];
-    [self.codingChallenge fetchObjects];
-    
-}
-
-- (void)objectsReadyToPick:(MSCodingChallenge *)codingChallenge
-{
-    MSObject *object = [self.codingChallenge pickObject];
-    NSLog(@"%@",[object description]);
 }
 
 - (void)didReceiveMemoryWarning {
